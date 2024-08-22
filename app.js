8cdbfc2188501d7523e1bacf6491c66073838273
Yash Singh <singhyash3012@gmail.com>
2024-08-22 16:59:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sendMail = require('./controllers/sendMail.js'); 
+const nodemailer = require('nodemailer');
 const cors = require('cors');
+require('dotenv').config();
+
+
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
+// Middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Endpoint to handle sending mail
-app.post('/sendMail', sendMail);
+// Nodemailer Transporter Setup
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EM_USER,
+        pass: process.env.EM_PASS,
+    },
+    pool: true,
+    maxConnections: 5,
+    maxMessages: 100,
+    rateLimit: 10,
+});
 
+// Routes
+app.post('/send-email', cors(), (req, res) => {
+    const { name, email, subject, phone, message } = req.body;
+
+    const mailOptions = {
+        from: `${email}`,
+        to: `${process.env.EM_USER}`,
+        subject: `${subject}` || `Contact Form Submission from ${name}`,
+        text: `You have received a new message from your website contact form.\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ error: 'Failed to send email' });
+        }
+        res.status(200).json({ success: 'Email sent successfully!' });
+    });
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server is running on port ${port}`);
 });
